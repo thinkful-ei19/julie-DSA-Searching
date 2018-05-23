70b35dd483619f3ee255caeb41ade6ca611634e1
Julie K <julieskim160@gmail.com>
2018-05-23 12:33:43 -0700

    find book and working on tree traversal

---
+++ data-search/src/form.js
@@ -0,0 +1,107 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: 'none',
+            count: 'none'
+        }
+      }
+      
+
+  
+      render(){
+          let string = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+          let arr = string.split(' ').sort((a,b) => {return a-b}).map((value) => value);
+          const newThis = this;
+          function linearSearch(string, value){
+            for(let i=0; i<string.length; i++){
+                if(arr[i] === value){
+                    newThis.setState({
+                    input: value,
+                    count: i
+                    })
+                }
+            }
+            console.log(value);
+        }
+        function binarySearch(arr, value, start=0, end= arr.length-1, count=0) {
+            count++;
+            
+        
+            if (start > end) {
+                bindThis.setState({
+                    input: 'Not found',
+                    count: 'none'
+                })
+                return;
+            }
+        
+            const index = Math.floor((start + end) / 2);
+            const item = arr[index];
+        
+            console.log(start, end);
+            if (item === value) {
+                bindThis.setState({
+                    input: value,
+                    count: count
+                })
+                return;
+            }
+            else if (item < value) {
+                return binarySearch(arr, value, index + 1, end, count);
+            }
+            else if (item > value) {
+                return binarySearch(arr, value, start, index - 1, count);
+            }
+        };
+          return (
+            <div>
+            <form onSubmit={event => {
+                event.preventDefault();
+                const input = event.target.number;
+                console.log(input);
+                
+        
+              }}>
+                <div className="textarea">
+                    <h1>Enter Number</h1>
+                    <label>Linear Search</label>
+                    <input onChange={(event)=> (linearSearch(arr, event.target.value))}/>
+                    <label>Binary Search</label>
+                    <input onChange={(event)=> (binarySearch(arr, event.target.value))}/>
+                    <p>Number: {this.state.input},</p>
+                    <p>Iterations: {this.state.count}</p>
+                </div>
+            </form>
+
+
+
+                <p>Find a Book</p>
+                <p>To find a book in the library with the Dewey Decimal index, first 
+                    you'd have to identify the books dewey. From there, use a binary search 
+                    method to cut down the database in half. Then see which half has the 
+                    number and get rid of the other half. Then repeat the process and check the middle 
+                    and eliminate the rest until the element is found. Resulting with logarithmic method 
+                    as the worst case. 
+                </p>
+            </div>
+          );
+      }
+}
+
+
+
+const library = [
+    { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+    { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+    { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+    { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+    { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+    { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+    { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+    { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+    { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+    { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+  ];
---
+++ drills.js
@@ -0,0 +1,169 @@
+//Tree Traversal
+//in-order
+//pre-order
+//post-order
+class BinarySearchTree {
+    constructor(key=null, value=null, parent=null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(key, value) {
+        //if the tree is empty then this key being inserted is the root node of the tree
+        if (this.key == null) {
+            this.key = key;
+            this.value = value;
+        }
+
+        // If the new key is less than the node's key 
+        // needs to live in the LEFT
+        else if (key < this.key) {
+            //if the existing node does not have any left child, 
+            //instantiate & insert new node  
+            if (this.left == null) {
+                this.left = new BinarySearchTree(key, value, this); //pass 'this' as parent
+            }
+            //if the node has an existing left child, 
+            //then we recursively call the `insert` method 
+            //so the node is added further down the tree.
+            else {
+                this.left.insert(key, value); //recursive shit right here
+            }
+        }
+        //Similarly, if the new key is greater than the node's key 
+        //then you do the same thing, but on the RIGHT side.
+        else {
+            if (this.right == null) {
+                this.right = new BinarySearchTree(key, value, this);
+            }
+            else {
+                this.right.insert(key, value);
+            }
+        }
+    }
+    find(key) {
+        //if the item is found at the root then return that value
+        if (this.key == key) {
+            return this.value;
+        }
+        //if the item less than the root go LEFT
+        //if there is an existing left child, 
+        //then recursively check its left and/or right child
+        else if (key < this.key && this.left) {
+            return this.left.find(key);
+        }
+        //if the item is greater than the root go RIGHT
+        //if there is an existing right child, 
+        //then recursively check its left and/or right child
+        //until you find the item.
+        else if (key > this.key && this.right) {
+            return this.right.find(key);
+        }
+        //You have search the treen and the item is not in the tree
+        else {
+            throw new Error('Key Error');
+        }
+    }
+    remove(key) {
+        if (this.key == key) {
+            if (this.left && this.right) {
+                const successor = this.right._findMin();
+                this.key = successor.key;
+                this.value = successor.value;
+                successor.remove(successor.key);
+            }
+            //If the node only has a left child, 
+            //then you replace the node with its left child.  
+            else if (this.left) {
+                this._replaceWith(this.left);
+            }
+            //And similarly if the node only has a right child 
+            //then you replace it with its right child.
+            else if (this.right) {
+                this._replaceWith(this.right);
+            }
+            //If the node has no children then
+            //simply remove it and any references to it 
+            //by calling "this._replaceWith(null)".
+            else {
+                this._replaceWith(null);
+            }
+        }
+        else if (key < this.key && this.left) {
+            this.left.remove(key);
+        }
+        else if (key > this.key && this.right) {
+            this.right.remove(key);
+        }
+        else {
+            throw new Error('Key Error');
+        }
+    }
+    _replaceWith(node) {
+        if (this.parent) {
+            if (this == this.parent.left) {
+                this.parent.left = node;
+            }
+            else if (this == this.parent.right) {
+                this.parent.right = node;
+            }
+
+            if (node) {
+                node.parent = this.parent;
+            }
+        }
+        else {
+            if (node) {
+                this.key = node.key;
+                this.value = node.value;
+                this.left = node.left;
+                this.right = node.right;
+            }
+            else {
+                this.key = null;
+                this.value = null;
+                this.left = null;
+                this.right = null;
+            }
+        }
+    }
+
+    _findMin() {
+        if (!this.left) {
+            return this;
+        }
+        return this.left._findMin();
+    }
+
+    _findMax() {
+        if(!this.right) {
+            return this;
+        }
+        return this.right._findMax();
+
+    }
+}
+
+const bst = '25 15 50 10 24 35 70 4 12 18 31 44 66 90 22';
+
+function main() {
+    let bst = new BinarySearchTree();
+        bst.insert(25);
+        bst.insert(15);
+        bst.insert(50);
+        bst.insert(10);
+        bst.insert(24);
+        bst.insert(35);
+        bst.insert(70);
+        bst.insert(4);
+        bst.insert(12);
+        bst.insert(18);
+        bst.insert(31);
+        bst.insert(44);
+        bst.insert(66);
+        bst.insert(90);
+        bst.insert(22);
+}
